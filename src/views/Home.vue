b2b7d2598f960aad57802261c26f60d09a3aa76c
You Song Hou <yousonghou@gmail.com>
2023-01-26 00:54:13 -0500

    feat: install and add chakra-ui

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <c-button variant-color="green">Button</c-button>
     <HelloWorld msg="Digital-Receipt"/>
   </div>
 </template>
 
 <script>
+
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
-
+ 
 export default {
   name: 'Home',
   components: {
