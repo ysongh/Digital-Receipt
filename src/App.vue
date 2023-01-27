4a72ce65d9f9629e7394db80df765a27daae6ea5
You Song Hou <yousonghou@gmail.com>
2023-01-26 22:25:21 -0500

    feat: add a Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <template>
   <c-theme-provider>
     <c-reset />
+    <Navbar />
     <router-view />
   </c-theme-provider>
 </template>
 
 <script>
+import Navbar from "./components/layout/Navbar.vue"
 
 export default {
   name: "App",
+  components: {
+    Navbar
+  },
 };
 </script>
---
+++ src/components/layout/Navbar.vue
@@ -0,0 +1,28 @@
+<template>
+  <c-box bg="#1dab2e" w="100%" p="4" color="white">
+    <c-flex align="center" justify="space-between" px="6">
+      <c-flex>
+        <c-link mr="4" as="router-link" to="/">
+          Digital Receipt
+        </c-link>
+        <c-link mr="4" as="router-link" to="/">
+          Home
+        </c-link>
+        <c-link as="router-link" to="/about">
+          About
+        </c-link>
+      </c-flex>
+      <c-button variant-color="yellow">Connect Wallet</c-button>
+    </c-flex>
+  </c-box>
+</template>
+
+<script>
+
+export default {
+  name: "Navbar"
+}
+</script>
+
+<style>
+</style>
