560894e96a0d4e03556b317ea235ac2378ee4c65
You Song Hou <yousonghou@gmail.com>
2023-01-28 01:10:14 -0500

    feat: update hardhat.config.js to deploy a contract on Filecoin Hyperspace Testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    // npx hardhat run scripts/deploy.js --network skaletest
+    skaletest: {
+      url: "https://api.hyperspace.node.glif.io/rpc/v0",
+      accounts: [process.env.PRIVATEKEY],
+      chainId: 3141,
+      gasPrice: 8000000000
+    },
+  }
 };
