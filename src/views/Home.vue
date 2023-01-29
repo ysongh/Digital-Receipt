85e85b20ac8a69c83235098ef1a85bb3a80a6c79
You Song Hou <yousonghou@gmail.com>
2023-01-29 01:07:01 -0500

    refactor: remove HelloWorld.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,19 +2,17 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <c-button variant-color="green">Button</c-button>
-    <HelloWorld msg="Digital-Receipt"/>
   </div>
 </template>
 
 <script>
 
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
  
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
