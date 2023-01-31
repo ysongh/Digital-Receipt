6a89eb3293b243261f7127809d9167cec246ab22
You Song Hou <yousonghou@gmail.com>
2023-01-30 21:58:49 -0500

    feat: connect client to contract

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import { ethers } from 'ethers'
 
+import { CONTRACT_ADDRESS }  from '../config'
+import DigitalReceipt from '../../artifacts/contracts/DigitalReceipt.sol/DigitalReceipt.json'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
@@ -9,10 +12,12 @@ export default new Vuex.Store({
     walletAddress: "",
   },
   getters: {
-    walletAddress: state => state.walletAddress
+    walletAddress: state => state.walletAddress,
+    DRContract: state => state.DRContract,
   },
   mutations: {
     setWalletAddress: (state, walletAddress) => (state.walletAddress = walletAddress),
+    setContract: (state, DRContract) => (state.DRContract = DRContract),
   },
   actions: {
     async connectToBlockchain({ commit }) {
@@ -20,6 +25,11 @@ export default new Vuex.Store({
         const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
         const account = ethers.utils.getAddress(accounts[0])
         commit('setWalletAddress', account)
+
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        const contract = new ethers.Contract(CONTRACT_ADDRESS, DigitalReceipt.abi, provider)
+        commit('setContract', contract)
+        console.log(contract)
       } catch(error) {
         console.log(error)
       }
