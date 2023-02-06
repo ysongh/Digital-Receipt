5af307830504094c6acbf7c0d432a38d4fb67fc7
You Song Hou <yousonghou@gmail.com>
2023-02-06 02:17:40 -0500

    feat: add Connect Wallet button in the Home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,33 @@
 <template>
   <c-box display="flex" flex-direction="column" align-items="center">
     <c-heading size="xl" mt="20">Digital Receipt</c-heading>
-    <c-text font-size="xl" mt="2">
+    <c-text font-size="xl" mt="2" mb="4">
       No more paper receipt
     </c-text>
-    <c-button mt="4" variant-color="yellow"  as="router-link" to="/add-receipt">
-      Send Receipt
+    <c-button v-if="!walletAddress" variant-color="yellow" @click="connectToBlockchain()">
+      Connect Wallet
     </c-button>
+    <div v-else>
+      <c-button variant-color="yellow" ml="1" mb="2" as="router-link" to="/add-receipt">
+        Send Receipt
+      </c-button>
+      <br>
+      <c-button variant-color="yellow"  as="router-link" to="/my-receipts">
+        View Receipts
+      </c-button>
+    </div>
+   
   </c-box>
 </template>
 
 <script>
-
-// @ is an alias to /src
+import { mapGetters, mapActions } from 'vuex'
  
 export default {
   name: 'Home',
-  components: {
-    
+  computed: mapGetters(['walletAddress']),
+  methods: {
+    ...mapActions(['connectToBlockchain'])
   }
 }
 </script>
