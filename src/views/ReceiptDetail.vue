a734f2c2ffe1bc408900336e5427b18d68c19acd
You Song Hou <yousonghou@gmail.com>
2023-02-10 00:51:11 -0500

    feat: add a back button

--- src/views/ReceiptDetail.vue
+++ src/views/ReceiptDetail.vue
@@ -1,28 +1,35 @@
 <template>
-  <c-box px="10" align="center">
-    <c-heading size="xl" my="5">Digital Receipt Detail</c-heading>
-    <c-box w="600px" align="left" border-width="1px" rounded="lg" overflow="hidden" p="6">
-      <c-box
-        mt="1"
-        font-weight="semibold"
-        as="h4"
-        line-height="tight"
-        is-truncated
-      >
-        Receipts #{{ receipt.id}}
-      </c-box>
-      <c-text font-size="lg" mt="2">
-        From: {{ receipt.from }}
-      </c-text>
-      <c-text font-size="lg" mt="2">
-        Total: {{ receipt.total }} {{ receipt.type }}
-      </c-text>
-      <c-text font-size="md" as="i" mt="2">
-        {{ receipt.description }}
-      </c-text>
+  <c-box px="10">
+    <c-heading size="xl" my="5" align="center">
+      Digital Receipt Detail
+    </c-heading>
+    <c-box align="center">
+      <c-button variant-color="yellow" mb="5" as="router-link" to="/my-receipts">
+        Back
+      </c-button>
+      <c-box w="600px" align="left" border-width="1px" rounded="lg" overflow="hidden" p="6">
+        <c-box
+          mt="1"
+          font-weight="semibold"
+          as="h4"
+          line-height="tight"
+          is-truncated
+        >
+          Receipts #{{ receipt.id}}
+        </c-box>
         <c-text font-size="lg" mt="2">
-        {{ receipt.dateNow }}
-      </c-text>
+          From: {{ receipt.from }}
+        </c-text>
+        <c-text font-size="lg" mt="2">
+          Total: {{ receipt.total }} {{ receipt.type }}
+        </c-text>
+        <c-text font-size="md" as="i" mt="2">
+          {{ receipt.description }}
+        </c-text>
+          <c-text font-size="lg" mt="2">
+          {{ receipt.dateNow }}
+        </c-text>
+      </c-box>
     </c-box>
   </c-box>
 </template>
